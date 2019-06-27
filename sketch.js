908a8457322bcdbea8832acdfcd2962824f26549
dallonsi <damien.allonsius@univ-amu.fr>
2019-06-27 14:32:28 +0200

    corrected a bug on the snake game. Now we can restart by cliking on the screen again.

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ let width = 300;
 let w = width / rez;
 let h = height / rez;
 let size = 10 / rez;
-let textsize = 50 / rez;
+let textsize = 30 / rez;
 let frameSpeed = 10;
 let play = false;
 let arrow_keys_handler = function(e) {
@@ -25,6 +25,7 @@ function setup() {
     snake = new Snake();
     textSize(textsize);
     textAlign(CENTER, CENTER);
+    noLoop();
 }
 
 function keyPressed() {
@@ -32,15 +33,15 @@ function keyPressed() {
     case UP_ARROW:
 	snake.setDir(createVector(0,-1));
 	break;
-	
+
     case DOWN_ARROW:
 	snake.setDir(createVector(0,1));
 	break;
-	
+
     case LEFT_ARROW:
 	snake.setDir(createVector(-1,0));
 	break;
-	
+
     case RIGHT_ARROW:
 	snake.setDir(createVector(1,0));
 	break;
@@ -48,31 +49,36 @@ function keyPressed() {
 }
 function mousePressed(){
     play = true;
+    snake = new Snake();
+    loop();
 }
 function draw() {
     background(220);
     scale(rez);
 
-    if (play){
-	
-	window.addEventListener("keydown", arrow_keys_handler, false);
-	
-	if (snake.eat()){
-	    snake.grow();
-	    frameSpeed ++;
-	    frameRate(frameSpeed);
-	    snake.makeFood();
-	}
+    if (play)
+    {
 
-	snake.move();
-	snake.show();
-	snake.showFood();
-	if (snake.end()){
-	    background(255, 0, 0);
-	    fill(0,255,0);
-	    text('You lose !\n' + 'score = ' + snake.score, w / 2, w /2);
-	    noLoop();
-	    window.removeEventListener("keydown", arrow_keys_handler, false);
-	}
+        window.addEventListener("keydown", arrow_keys_handler, false);
+
+        if (snake.eat())
+        {
+                snake.grow();
+                frameSpeed ++;
+                frameRate(frameSpeed);
+                snake.makeFood();
+        }
+
+        snake.move();
+        snake.show();
+        snake.showFood();
+        if (snake.end())
+        {
+            background(102,203,140);
+            text('You lose !\n' + 'score = ' + snake.score, w / 2, w /2);
+            noLoop();
+            window.removeEventListener("keydown", arrow_keys_handler, false);
+            play = false;
+        }
     }
 }
